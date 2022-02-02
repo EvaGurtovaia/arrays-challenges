5fbf9102c1f169a42db823cc679bfda1436cb193
EvaGurtovaia <eva.gurtovaya@gmail.com>
2022-02-02 13:07:42 -0600

    One more way of solving merge and sort arrays  challenge

--- index.js
+++ index.js
@@ -20,9 +20,41 @@ console.log(reverseTwo("Hi, My name is Eva"));
 
 //merge and sort two arrays
 
-const merge = (arr1, arr2) => {
+const mergeTwo = (arr1, arr2) => {
     const arr3 = arr1.concat(arr2);
     return arr3.sort();
 };
 
 console.log(merge([1, 2, 4], [3, 6, 5]));
+
+const merge = (arr1, arr2) =>{
+    if (arr1.length === 0){
+  return arr2;
+    }
+    if (arr2.length === 0){
+      return arr1;
+    }
+    
+   const mergedArray = [];
+    let arr1Item = arr1[0];
+    let arr2Item = arr2[0];
+    let i = 1;
+    let j = 1;
+    
+    while (arr1Item || arr2Item){
+      if (! arr2Item || arr1Item < arr2Item){
+          mergedArray.push(arr1Item);
+     arr1Item = arr1[i];
+      i++;
+      }
+    else {
+      mergedArray.push(arr2Item);
+     arr2Item = arr2[j];
+      j++;
+    }
+    }
+   return mergedArray; 
+  }
+  
+  
+  console.log(mergeTwo([1,2,4],[3,5,6]));
