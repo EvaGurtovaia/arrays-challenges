c9e680988be4993ba0ab5b04498a9bb0f6d3aa63
EvaGurtovaia <eva.gurtovaya@gmail.com>
2022-02-09 17:22:49 -0600

    Unique values challenge

--- index.js
+++ index.js
@@ -258,3 +258,15 @@ const isAnagram = (s, t) => {
     }
     return true;
 };
+
+//Implement a function calle countUniqueValues, which accepts a sorted array
+//and count the unique values in the array. There can be negative numbers in the array,
+//but it will always be sorted.
+
+const countUniqueValues = arr => {
+    let result = new Set(arr);
+    return result.size
+  
+ }
+     
+ console.log(countUniqueValues([1,2,3,4,4,4,7,7,12,12,13]));  //7
