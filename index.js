56bd3e317549c79bb0335d56a0374f3a62c1d227
EvaGurtovaia <eva.gurtovaya@gmail.com>
2022-02-10 14:33:23 -0600

    Squares of a sorted array challenge

--- index.js
+++ index.js
@@ -299,3 +299,24 @@ const findMaxAverage = (nums, k) => {
 };
 
 console.log(findMaxAverage([1, 12, -5, -6, 50, 3], 4));
+
+// Squares of a Sorted Array
+
+//Given an integer array nums sorted in non-decreasing order,
+//return an array of the squares of each number sorted in non-decreasing order.
+
+//Input: nums = [-4,-1,0,3,10]
+//Output: [0,1,9,16,100]
+//Explanation: After squaring, the array becomes [16,1,0,9,100].
+//After sorting, it becomes [0,1,9,16,100].
+
+const sortedSquares = (nums) => {
+    let squaredArr = [];
+    for (let num of nums) {
+        squaredArr.push(num * num);
+    }
+    let sortedSquares = squaredArr.sort((a, b) => a - b);
+    return sortedSquares;
+};
+
+console.log(sortedSquares([-4, -1, 0, 3, 10]));
